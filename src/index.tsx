051c18ca78508ea987d977cfce821ee5c8800cfc
yuto ichinose <yuto.0526.sano@gmail.com>
2023-01-11 13:12:45 +0900

    pwaやってみた、駄目そうなら戻す

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ChakraProvider} from '@chakra-ui/react'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+serviceWorkerRegistration.unregister();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
